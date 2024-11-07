d1e969add8c67b1f01d7a322ee079847291cf1f8
root <root@abdeldroid.>
2024-11-07 11:41:42 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,56 +2,48 @@ import sys
 import os
 import zlib
 import hashlib
-def init():
-    os.mkdir(".git")
-    os.mkdir(".git/objects")
-    os.mkdir(".git/refs")
-    with open(".git/HEAD", "w") as f:
-        f.write("ref: refs/heads/master\n")
-    print("Initialized git directory")
-def cat_file(object_type, sha):
-    current_directory = os.getcwd()
-    path = f"{current_directory}/.git/objects/{sha[0:2]}/{sha[2:]}"
-    with open(path, "rb") as file:
-        file_contents = file.read()
-    decompressed_file = zlib.decompress(file_contents)
-    first_space = decompressed_file.find(b" ")
-    object_type = str(decompressed_file[0:first_space])
-    decompressed_file = decompressed_file[first_space + 1 :]
-    null_value = decompressed_file.find(b"\x00")
-    size_of_blob = str(decompressed_file[0:null_value])
-    content = decompressed_file[null_value + 1 :].decode("ascii")
-    print(content, end="")
-def hash_object(object_type, file_name):
-    with open(file_name, "rb") as file:
-        file_content = file.read()
-    sha = hashlib.sha1(file_content).hexdigest()
-    header = f"blob {len(file_content)}\x00"
-    store = header.encode("ascii") + file_content
-    sha = hashlib.sha1(store).hexdigest()
-    git_path = os.path.join(os.getcwd(), ".git/objects")
-    os.mkdir(os.path.join(git_path, sha[0:2]))
-    with open(os.path.join(git_path, sha[0:2], sha[2:]), "wb") as file:
-        file.write(zlib.compress(store))
-    print(sha)
 def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    # print("Logs from your program will appear here!")
+    # Uncomment this block to pass the first stage
     command = sys.argv[1]
     if command == "init":
-        init()
+        os.mkdir(".git")
+        os.mkdir(".git/objects")
+        os.mkdir(".git/refs")
+        with open(".git/HEAD", "w") as f:
+            f.write("ref: refs/heads/main\n")
+        print("Initialized git directory")
     elif command == "cat-file":
-        if len(sys.argv) != 4:
-            print("usage: git cat-file <type> <object>", file=sys.stderr)
-            exit()
-        object_type = sys.argv[2]
-        sha = sys.argv[3]
-        cat_file(object_type, sha)
+        if sys.argv[2] == "-p":
+            blob_sha = sys.argv[3]
+            with open(f".git/objects/{blob_sha[:2]}/{blob_sha[2:]}", "rb") as f:
+                raw = zlib.decompress(f.read())
+                header, content = raw.split(b"\0", maxsplit=1)
+                print(content.decode("utf-8"), end="")
     elif command == "hash-object":
-        if len(sys.argv) != 4:
-            print("usage: hash-object -w <file>", file=sys.stderr)
-            exit()
-        object_type = sys.argv[2]
-        file_name = sys.argv[3]
-        hash_object(object_type, file_name)
+        if sys.argv[2] == "-w":
+            file = sys.argv[3]
+            with open(file, "rb") as f:
+                data = f.read()
+                header = f"blob {len(data)}\0".encode("utf-8")
+                store = header + data
+                sha = hashlib.sha1(store).hexdigest()
+                os.makedirs(f".git/objects/{sha[:2]}", exist_ok=True)
+                with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as f:
+                    f.write(zlib.compress(store))
+                    print(sha)
+    elif command == "ls-tree":
+        param, hash = sys.argv[2], sys.argv[3]
+        if param == "--name-only":
+            with open(f".git/objects/{hash[:2]}/{hash[2:]}", "rb") as f:
+                data = zlib.decompress(f.read())
+                _, binary_data = data.split(b"\x00", maxsplit=1)
+                while binary_data:
+                    mode, binary_data = binary_data.split(b"\x00", maxsplit=1)
+                    _, name = mode.split()
+                    binary_data = binary_data[20:]
+                    print(name.decode("utf-8"))
     else:
         raise RuntimeError(f"Unknown command #{command}")
 if __name__ == "__main__":
